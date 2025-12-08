83b286591d36620b32a0d434987be1ad9613b981
Dan Fellini <dan@nten.org>
2025-12-08 10:54:59 -0800

    wip

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,12 +5,12 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - BugMoto',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'SBugMoto - Motorcycles and adventures.',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'Whether you’re a seasoned rider, a returning motorcyclist, or just someone who loves the open road, this is a space for inspiring stories, insightful videos, and a few laughs along the way.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,7 +26,8 @@ import { FaMotorcycle } from "react-icons/fa";
 
 
 const videos = await fetch(
-  `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-GaHIsTlT1KUt5c4yWNUDA&maxResults=5&order=date&type=video&key=AIzaSyBrbXSd6TM_UMs4SxCWmcfbuxb3Jg7gfKA`
+  `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-GaHIsTlT1KUt5c4yWNUDA&maxResults=5&order=date&type=video&key=${process.env.YOUTUBE_KEY}`,
+  { next: { revalidate: 180 } }
 ).then((res) => res.json());
 
 export default async function Home() {
